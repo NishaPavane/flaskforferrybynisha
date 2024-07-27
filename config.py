c58ec3857dd3e97a82390e8cb800ef6a725b9f02
NISHA PAVANE <nishapavane65@gmail.com>
2024-07-27 15:07:06 +0530

    second commit

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ password = 'Khoji@123'
 encoded_password = urllib.parse.quote_plus(password)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'#'mysql+pymysql://root:11052003%40nisha@localhost/flask_fdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:11052003%40nisha@localhost/flask_fdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'sanjivani@nisha11'
     SQLALCHEMY_DATABASE_URI_auth = f'mysql+pymysql://khoji:{encoded_password}@144.24.96.48/FerryOne'
